95993e78e8f134ef4239d86d02dae56ba1419fb0
think24 <think24@think24s-MacBook-Pro.local>
2017-05-12 15:53:01 +0530

    Video Url

--- VideoPlayer/VideoCollectionViewCell.swift
+++ VideoPlayer/VideoCollectionViewCell.swift
@@ -7,7 +7,65 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
-class VideoCollectionViewCell: UICollectionViewCell {
+class VideoCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet weak var videoView: UIView!
+    var videoPlayer : AVPlayerViewController?
+    var avPlayerController : AVPlayerViewController?
+    var paused: Bool = false
+    var videoUrl: String? = nil {
+        didSet {
+            /*
+             If needed, configure player item here before associating it with a player.
+             (example: adding outputs, setting text style rules, selecting media options)
+             */
+            
+            self.setupMoviePlayer()
+        }
+    }
     
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+    
+    func setupMoviePlayer()
+    {
+        self.avPlayerController = AVPlayerViewController()
+        let avPlayer = AVPlayer.init(playerItem: AVPlayerItem(url: URL(string: videoUrl!)!))
+        avPlayer.volume = 3
+        avPlayer.actionAtItemEnd = .none
+        avPlayerController?.player = avPlayer
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.playerItemDidReachEnd(notification:)),
+                                               name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                                               object: avPlayer.currentItem)
+        avPlayerController?.view.frame = self.videoView.bounds
+        self.avPlayerController?.videoGravity = AVLayerVideoGravityResizeAspect
+        self.backgroundColor = .clear
+        
+        for view in self.videoView.subviews
+        {
+            view.removeFromSuperview()
+        }
+        self.videoView.insertSubview(avPlayerController!.view, at: 0)
+    }
+    
+    func stopPlayback()
+    {
+        self.avPlayerController?.player?.pause()
+    }
+    
+    func startPlayback(){
+        self.avPlayerController?.player?.play()
+    }
+    
+    // A notification is fired and seeker is sent to the beginning to loop the video again
+    func playerItemDidReachEnd(notification: Notification) {
+        let p: AVPlayerItem = notification.object as! AVPlayerItem
+        p.seek(to: kCMTimeZero)
+    }
 }
--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -8,18 +8,88 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout
+{
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var visibleIP : IndexPath = IndexPath.init(row: 0, section: 0)
+    var aboutToBecomeInvisibleCell = -1
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        if let videoCell = collectionView.cellForItem(at: visibleIP) as? VideoCollectionViewCell
+        {
+            videoCell.startPlayback()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool)
+    {
+        if let videoCell = collectionView.cellForItem(at: visibleIP) as? VideoCollectionViewCell
+        {
+            videoCell.stopPlayback()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "videoCell", for: indexPath) as! VideoCollectionViewCell
+        cell.videoUrl = "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        return CGSize(width: collectionView.frame.size.width, height: 200)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView)
+    {
+         let visibleIndexPaths = collectionView.indexPathsForVisibleItems
+        if visibleIndexPaths.count > 0
+        {
+            var indexPathVissibled : Bool = false
+            for indexPath in visibleIndexPaths
+            {
+                if let videoCell = collectionView.cellForItem(at: indexPath) as? VideoCollectionViewCell
+                {
+                    if collectionView.bounds.contains(videoCell.frame) && !indexPathVissibled
+                    {
+                        videoCell.startPlayback()
+                        self.visibleIP = indexPath
+                        indexPathVissibled = true
+                    }
+                    else
+                    {
+                        aboutToBecomeInvisibleCell = indexPath.item
+                        videoCell.stopPlayback()
+                    }
+                }
+            }
+        }        
+    }
 }
 
