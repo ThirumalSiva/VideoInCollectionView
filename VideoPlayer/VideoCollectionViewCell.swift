7e92900037d6244690851067fdff46f1e6b12c1f
think24 <think24@think24s-MacBook-Pro.local>
2017-05-12 15:47:48 +0530

    first commit

---
+++ VideoPlayer/VideoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VideoCollectionViewCell.swift
+//  VideoPlayer
+//
+//  Created by think24 on 5/12/17.
+//  Copyright © 2017 Think42Labs. All rights reserved.
+//
+
+import UIKit
+
+class VideoCollectionViewCell: UICollectionViewCell {
+    
+}
